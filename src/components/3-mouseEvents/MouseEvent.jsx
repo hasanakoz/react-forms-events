ae69198112aabad6ef395fc6aea9a1ad3a4f570c
hasan <hasanakoz1986@gmail.com>
2022-10-13 23:06:02 +0300

    onMouseEnter, onMouseOut, onDoubleClick events

--- src/components/3-mouseEvents/MouseEvent.jsx
+++ src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
+
 const MouseEvent = () => {
+  const [showHint, setShowHint] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const handleDoubleClick = (e) => {
+    setToggle(!toggle);
+    toggle
+      ? (e.target.className = "bg-danger text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-success text-light w-50 p-4 mt-4");
+  };
+
+  // const handleMouseMove = (e) = {
+  //   setCoordX(e.nativeEvent.offsetX)
+  //   setCoordY(e.nativeEvent.offsetY)
+  // }
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
 
-      <div id="todo-1" className="bg-success text-light w-50 p-4">
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseEnter={() => setShowHint(true)}
+        onMouseOut={() => setShowHint(false)}
+      >
         todo item 1
       </div>
+      {showHint && <p>Hello React</p>}
 
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
         todo item 2
       </div>
 
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        // onMouseMove={handleMouseMove}
+      >
         todo item 3
       </div>
 
