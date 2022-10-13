fb8ac9445a1e99b592e3b49cefe3e18933b0d334
hasan <hasanakoz1986@gmail.com>
2022-10-13 22:02:42 +0300

    prevent entering numeric inputs

--- src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -2,6 +2,17 @@ import { useState } from "react";
 
 const KeyboardClipboard = () => {
   const [inputValue, setInputValue] = useState("");
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if (
+      (e.keyCode >= 48 && e.keyCode <= 57) ||
+      (e.keyCode >= 96 && e.keyCode <= 105)
+    ) {
+      alert("please dont enter number!");
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="container text-center">
       <h1>CLIPBOARD EVENTS</h1>
@@ -12,6 +23,7 @@ const KeyboardClipboard = () => {
           e.target.value = e.target.value.toLocaleUpperCase();
           setInputValue(e.target.value);
         }}
+        onKeyDown={handleKeyDown}
       />
 
       <p className="text-start mt-4">{inputValue}</p>
